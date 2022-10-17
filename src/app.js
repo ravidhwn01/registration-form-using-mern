9072bb4d67607301ca0849359a587ca8b7140912
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-17 21:50:57 +0530

    uses hbs

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
-const express  = require("express")
+const express = require("express");
 const app = express();
-const path = require("path")
-const port = process.env.PORT ||3000;
-require("../src/db/conn")
-const static_path = path.join(__dirname,"../public")
-app.use(express.static(static_path))
-// console.log(path.join(__dirname));
-app.get("/",(req,res)=>{
-    res.send("hello from the dark side!")
-})
+const path = require("path");
+const hbs = require("hbs");  // for using partials
+require("../src/db/conn");
+const port = process.env.PORT || 3000;
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+app.use(express.static(static_path));
+app.use(express.static('views'));
+app.engine('html', require('hbs').__express);
+app.set('views', 'views');
+app.set('view engine', 'hbs');
+app.set("views", template_path); // templates m hai views
+hbs.registerPartials(partials_path);
+console.log(path.join(template_path));
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-
-app.listen(port,()=>{
-    console.log(`server is running at port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server is running at port ${port}`);
+});
